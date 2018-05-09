dc21499fed3b4ac49e497f103710ced0658668d1
zhangheying <15001082281@163.com>
2018-05-09 17:11:23 +0800

    Initial commit

---
+++ src/main/java/com/zhy/fs/sf.java
@@ -0,0 +1,12 @@
+package com.zhy.fs;
+
+/**
+ * Somnus
+ * sf  2018/5/9  17:09
+ **/
+
+public class sf {
+    public static void main(String[] args) {
+        System.out.println("hahahah哈哈哈");
+    }
+}
